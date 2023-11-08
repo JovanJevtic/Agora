33ec06773a23e4630608b2865c89c6be4ddf6449
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-11-08 02:23:04 +0100

    cors

--- src/app/components/TrendingNewsThumbnail/TrendingNewsThumbnail.tsx
+++ src/app/components/TrendingNewsThumbnail/TrendingNewsThumbnail.tsx
@@ -1,12 +1,20 @@
 import { Post } from '@prisma/client'
 import React from 'react'
 import TrendingNewsDate from '../TrendingNews/TrendingNewsDate';
+import { Skeleton } from '../ui/skeleton';
 
 type Props = {
     post: Post;
     big: boolean;
 }
 const TrendingNewsThumbnail: React.FunctionComponent<Props> = ({ post, big }) => {
+
+    if (!post || !post.image) {
+        return (
+            <Skeleton className='h-full w-full' />
+        )
+    }
+
   return (
     <div className='h-full w-full'>
         <div className={`relative h-full trending-news-th-image`} style={{ backgroundImage: `url(${post.image})`, backgroundPosition: 'center' }}>
