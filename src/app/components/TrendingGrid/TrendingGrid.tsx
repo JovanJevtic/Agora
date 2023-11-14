87acb22d559434369c1e4ee58375d75896f1961d
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-11-14 18:32:29 +0100

    redesign

--- src/app/components/TrendingGrid/TrendingGrid.tsx
+++ src/app/components/TrendingGrid/TrendingGrid.tsx
@@ -9,20 +9,21 @@ type Props = {
 const TrendingGrid: React.FunctionComponent<Props> = ({ posts }) => {
   return (
     <div className='flex w-full mt-3 mb-10 h-[700px] md:h-[350px]'>
-        <div className='grid h-full w-full grid-cols-2 grid-rows-3 md:grid-cols-4 md:grid-rows-2 gap-3'>
-            <Link href={`/post/${posts[0].id}`} className='col-start-1 col-span-2 row-span-1 md:col-span-2 md:row-span-2'>
+        <div className='grid h-full w-full grid-cols-2 grid-rows-10 md:grid-cols-4 md:grid-rows-2 gap-3'>
+           
+            <Link href={`/post/${posts[0].id}`} className='col-start-1 col-span-2 row-span-6 md:row-span-10 md:col-span-2 md:row-span-2'>
                 <TrendingNewsThumbnail big post={posts[0]} />
             </Link>
-            <Link href={`/post/${posts[1].id}`} className='col-span:1 row-span-1'>
+            <Link href={`/post/${posts[1].id}`} className='col-span-2 row-span-1  md:col-span-1'>
                 <TrendingNewsThumbnail big={false} post={posts[1]} />
             </Link> 
-            <Link href={`/post/${posts[2].id}`} className='col-span:1 row-span-1'>
+            <Link href={`/post/${posts[2].id}`} className='col-span-2 row-span-1  md:col-span-1'>
                 <TrendingNewsThumbnail big={false} post={posts[2]} />
             </Link>
-            <Link href={`/post/${posts[3].id}`} className='col-span:1 row-span-1'>
+            <Link href={`/post/${posts[3].id}`} className='col-span-2 row-span-1  md:col-span-1'>
                 <TrendingNewsThumbnail big={false} post={posts[3]} />
             </Link>
-            <Link href={`/post/${posts[4].id}`} className='col-span:1 row-span-1'>
+            <Link href={`/post/${posts[4].id}`} className='col-span-2 row-span-1  md:col-span-1'>
                 <TrendingNewsThumbnail big={false} post={posts[4]} />
             </Link>
         </div>  
--- src/app/components/TrendingNewsThumbnail/TrendingNewsThumbnail.tsx
+++ src/app/components/TrendingNewsThumbnail/TrendingNewsThumbnail.tsx
@@ -6,6 +6,7 @@ import TrendingNewsDate from '../TrendingNews/TrendingNewsDate';
 import { Skeleton } from '../ui/skeleton';
 import Image from 'next/image';
 import { Category } from '@prisma/client';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 
 type Props = {
     post: Post;
@@ -46,8 +47,75 @@ const TrendingNewsThumbnail: React.FunctionComponent<Props> = ({ post, big }) =>
     }
 
   return (
-    <div className='h-full w-ful'>
-    <div className='relative w-full h-full'>
+    <>
+        <div className='h-full w-full md:hidden'>
+        {
+            big ? <div className='relative w-full h-full'>
+                <Image 
+                    alt='thumbnail'
+                    src={post.image}
+                    layout='fill'
+                    objectFit='cover'
+                />
+                <div className="absolute h-full w-full opacity-100 flex flex-col">
+                    <div className='h-[90%]' style={{ backgroundImage: 'linear-gradient(180deg, transparent, black)' }}></div>
+                    <div className='bg-black h-[10%]'></div>
+                </div>
+                <div className='absolute h-full w-full bg-transparent'>
+                    <div className='h-full w-full pl-3 pr-3 md:pl-5 md:pr-5 flex flex-col justify-end'>
+                        <h1 className={`mb-0 ml-0 line-clamp-2
+                            text-xl
+                            font-bold
+                            md:text-2xl
+                            lg:text-3xl
+                        `}>{post.title}</h1>
+                        { big && <p className='text-xs w-[70%] lg:text-sm mb-1 mt-0.5 text-gray-400 line-clamp-2'>{post.subtitle}</p> }
+                        
+                        <div className='flex w-full items-center mb-3 mt-2'>
+                            <div className="flex items-center mt-0.5 flex-1">
+                                {
+                                    category ? 
+                                    <div className="flex mb-0 items-center mt-0">
+                                        <div className={`h-4 mr-1 w-[1px] ${ category.name === "Novosti" ? "bg-yellow-500" : category.name === "Sport" ? "bg-green-500" : category.name === "Kultura" ? "bg-purple-700" : category.name === "Drustvo" ? "bg-blue-500" : category.name === "Politika" ? "bg-red-500" :  "bg-white" }`}></div>
+                                        <p className="text-sm max-[700px]:text-xs" 
+                                        >{category.name}</p>
+                                    </div>
+                                    : loadingSub ?
+                                    <Skeleton className="h-4 w-12" /> : <></>
+                                }
+                            </div>
+                            <p className='md:text-xs text-gray-400 text-xs'><TrendingNewsDate date={post.createdAt} /></p>
+                        </div>
+                    </div>
+                </div>
+            </div> :
+            <div className='h-full w-full bg-card rounded-lg flex flex-col items-start pl-3 pr-3 justify-center'>
+                <h1 className={`mb-1.5 ml-0 line-clamp-2
+                            text-lg
+                            md:text-2xl
+                            lg:text-3xl
+                `}>{post.title}</h1>
+                <div className='flex w-full items-center'>
+                            <div className="flex items-center mt-0.5 flex-1">
+                                {
+                                    category ? 
+                                    <div className="flex mb-0 items-center mt-0">
+                                        <div className={`h-4 mr-1 w-[1px] ${ category.name === "Novosti" ? "bg-yellow-500" : category.name === "Sport" ? "bg-green-500" : category.name === "Kultura" ? "bg-purple-700" : category.name === "Drustvo" ? "bg-blue-500" : category.name === "Politika" ? "bg-red-500" :  "bg-white" }`}></div>
+                                        <p className="text-sm max-[700px]:text-xs" 
+                                        >{category.name}</p>
+                                    </div>
+                                    : loadingSub ?
+                                    <Skeleton className="h-4 w-12" /> : <></>
+                                }
+                            </div>
+                            <p className='md:text-xs text-gray-400 text-xs'><TrendingNewsDate date={post.createdAt} /></p>
+                        </div>
+
+            </div>
+        }
+        </div>
+        <div className='h-full w-ful hidden md:block'>
+            <div className='relative w-full h-full'>
             <Image 
                 alt='thumbnail'
                 src={post.image}
@@ -83,7 +151,8 @@ const TrendingNewsThumbnail: React.FunctionComponent<Props> = ({ post, big }) =>
                 </div>
             </div>
         </div>
-    </div>
+        </div>
+    </>
   )
 }
 
