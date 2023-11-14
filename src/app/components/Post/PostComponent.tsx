946ab20c91101340121c484938ba726c3541e977
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-11-14 10:53:41 +0100

    dizajn

--- src/app/components/Post/PostComponent.tsx
+++ src/app/components/Post/PostComponent.tsx
@@ -32,13 +32,13 @@ const Post: React.FunctionComponent<Props> = async ({ post }) => {
                 <article className="w-full prose-neutral prose-p:mt-0 prose-p:mb-0 prose-p:pt-0 lg:prose-xl prose-h1:leading-tight prose-h1:mb-0 prose-h1:pb-0">
                     <h1 className='text-3xl text-white font-bold'>{post.title}</h1>
                    
-                    <div className="flex w-full items-center mt-5">
-                        <div className="flex items-center mr-10">
+                    <div className="flex w-full min-[600px]:items-center mt-5 max-[1024px]:mb-5 max-[600px]:flex-col">
+                        <div className="flex items-center mr-10 max-[600px]:mb-1">
                             {
                                 (author.image) ? <Image className='mr-2' style={{borderRadius: '50%'}} src={author.image} height={24} width={24} alt="profile" />
                                 :<FaUserCircle className='mr-3' />
                             }
-                            <p className='text-sm max-[600px]:hidden'>{author.name}</p>
+                            <p className='text-sm'>{author.name}</p>
                         </div>
 
                         <div className="flex-1 flex items-center">
