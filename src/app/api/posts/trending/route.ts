6addd8408ced7f5a5fa4795065254a7c3a12a4b1
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-11-15 13:48:50 +0100

    asd

--- src/app/api/posts/trending/route.ts
+++ src/app/api/posts/trending/route.ts
@@ -5,14 +5,13 @@ export const GET = async (request: NextRequest) => {
     try {
         const posts = await prisma.post.findMany({ 
             where: {
-                AND: [
-                    { categoryId: 'clop313zk0000ju089a48am77'},
-                    { OR: [
-                        { position1: true }, { position2: true }, { position3: true }, { position4: true }, { position5: true }
-                    ] }
-                ]
-                
-                    
+                // AND: [
+                //     { categoryId: 'clop313zk0000ju089a48am77'},
+                //     { OR: [
+                //         { position1: true }, { position2: true }, { position3: true }, { position4: true }, { position5: true }
+                //     ] }
+                // ]
+                categoryId: 'clop346my0000r5dwlcpl9uf4'
                 // OR: [
                 //     { position1: true }, { position2: true }, { position3: true }, { position4: true }, { position5: true }
                 // ] 
