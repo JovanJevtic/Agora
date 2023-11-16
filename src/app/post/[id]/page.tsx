9ea46f05f5f96951af176e2699e0cc7c659605fc
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-11-16 17:22:55 +0100

    aa

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -58,20 +58,21 @@ export async function generateStaticParams() {
     }))
 }
 
-export async function generateMetadata(
-    { params }: Props,
-  ): Promise<Metadata> {
-    // read route params
-    const id = params.id
+// export async function generateMetadata(
+//     { params }: Props,
+//   ): Promise<Metadata> {
+//     // read route params
+//     const id = params.id
    
-    // fetch data
-    const post: Post = await fetch(`https://www.agoraportal.net/api/posts/getOne?id=${id}`).then((res) => res.json())
+//     // fetch data
+//     const post: Post = await fetch(`https://www.agoraportal.net/api/posts/getOne?id=${id}`).then((res) => res.json())
+//     console.log(post);
 
-    return {
-      title: post.title,
-      description: post.subtitle
-    }
-  }
+//     return {
+//       title: post.title,
+//     //   description: post.subtitle
+//     }
+//   }
 
 const Page: React.FunctionComponent<Props> = async ({ params: { id } }) => {
     const postData: Promise<Post> = getPost(id);
