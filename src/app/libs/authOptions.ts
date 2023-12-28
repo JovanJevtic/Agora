e00f111f1061daa767bb5721d2bee2376b848040
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-12-29 00:12:43 +0100

    fix: @next-auth adapter instead of @auth

--- src/app/libs/authOptions.ts
+++ src/app/libs/authOptions.ts
@@ -3,7 +3,7 @@ import { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import { compare } from 'bcryptjs'
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
