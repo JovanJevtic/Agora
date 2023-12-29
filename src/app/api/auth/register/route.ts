5969813bcc131b981832f034d063093fe250c3a6
Jovan Jevtic <jovanjevtic132@gmail.com>
2023-12-29 03:44:06 +0100

    fix: mail sening

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -57,7 +57,8 @@ export const POST = async (request: NextRequest) => {
 
         if (!emailVerifyToken) return NextResponse.json({ error: 'Doslo je do greske...Molimo Vas pokusajte ponovo' }, { status: 500 });
 
-        const url = `${process.env.BASE_URL}/account/verify-email?token=${token}`
+        // const url = `${process.env.BASE_URL}/account/verify-email?token=${token}`
+        const url = `https://www.agoraportal.net/account/verify-email?token=${token}`
 
         const transporter = nodemailer.createTransport({ 
             service: 'gmail',
