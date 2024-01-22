279a96c8972040bb0f7d308d9c35c99210b74523
Jovan Jevtic <jovanjevtic132@gmail.com>
2024-01-22 01:33:29 +0100

    weird export issue

--- src/app/api/posts/all/staticParams/route.ts
+++ src/app/api/posts/all/staticParams/route.ts
@@ -2,14 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
 export const GET = async (request: NextRequest) => {
-    try {
-        const posts = await prisma.post.findMany({
-            where: {
-                archived: false
-            }
-        });
-        return NextResponse.json(posts.map(post => post.slug), { status: 200 });
-    } catch (error) {
-        return NextResponse.json({ error: error }, { status: 500 });
-    }
-}
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        archived: false,
+      },
+    });
+    return NextResponse.json(
+      posts.map((post) => post.slug),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error, "<<<<<<<errr");
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+};
